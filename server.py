85e0d92a0846f3d55016fe3143b594b6b736eca5
John Yang <byjohnyang@gmail.com>
2025-11-02 09:52:29 -0800

    Add freezer generators

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 import json
 import os
-import pathlib
-import shutil
 import sys
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 from flaskext.markdown import Markdown
@@ -27,8 +24,8 @@ pages = FlatPages(app)
 freezer = Freezer(app)
 markdown_manager = Markdown(app, extensions=['fenced_code'], output_format='html5',)
 
-pages_insights = [page for page in list(pages) if page.path.startswith('insights/')]
-pages_arenas = [page for page in list(pages) if page.path.startswith('arenas/')]
+pages_insights = [p for p in pages if p.path.startswith('insights/')]
+pages_arenas = [p for p in pages if p.path.startswith('arenas/')]
 
 # Routes
 @app.route('/')
@@ -44,22 +41,45 @@ def page(path):
 def team():
     return render_template('team.html')
 
-@app.route('/insights')
+@app.route('/insights/')
 def insights():
     return render_template('insights.html', pages=pages_insights)
 
-@app.route('/arenas')
+@app.route('/insights/<path:path>/')
+def insight(path):
+    return render_template('page.html', page=pages.get_or_404('insights/' + path))
+
+@app.route('/arenas/')
 def arenas():
     return render_template('arenas.html', pages=pages_arenas)
 
+@app.route('/arenas/<path:path>/')
+def arena(path):
+    return render_template('page.html', page=pages.get_or_404('arenas/' + path))
+
 @app.errorhandler(404)
 def page_not_found(path):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
+# Freezer generators
+@freezer.register_generator
+def page():
+    for p in pages:
+        if not p.path.startswith(('insights/', 'arenas/')):
+            yield {'path': p.path}
+
+@freezer.register_generator
+def insight():
+    for p in pages_insights:
+        yield {'path': p.path[9:]}  # Strip 'insights/' prefix
+
+@freezer.register_generator
+def arena():
+    for p in pages_arenas:
+        yield {'path': p.path[7:]}  # Strip 'arenas/' prefix
 
 @freezer.register_generator
 def static():
-    # This will yield all static files for freezing
     for dirpath, dirnames, filenames in os.walk('static'):
         for filename in filenames:
             rel_dir = os.path.relpath(dirpath, 'static')
@@ -68,7 +88,6 @@ def static():
 
 
 if __name__ == "__main__":
-    print(pages)
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
